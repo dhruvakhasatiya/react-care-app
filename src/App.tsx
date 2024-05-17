3afd7dd5504cc891b96616d7a36adc474389fc5f
Dhruva-Khasatiya <dhruvakhasatiya@gmail.com>
2024-05-17 14:21:06 +0530

    Calculate Journey

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent } from "react";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [firstEntry, setFirstEntry] = useState<string>("");
+  const [secondEntry, setSecondEntry] = useState<string>("");
+  const [result, setResult] = useState<string>("");
+
+  const handleFirstEntryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFirstEntry(e.target.value);
+  };
+
+  const handleSecondEntryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSecondEntry(e.target.value);
+  };
+
+  const handleJourneyCalculation = async () => {
+    if (firstEntry && secondEntry) {
+      const route: string = `${firstEntry},${secondEntry}`;
+      await axios
+        .get(
+          `https://media.carecontrolsystems.co.uk/Travel/JourneyPlan.aspx?Route=${route}`
+        )
+        .then((response) => setResult(response.data))
+        .catch((error) => console.log(error));
+    } else {
+      alert("Please enter both entry points.");
+    }
+  };
+
+  const displayResult = () => (
+    <>
+      <h3>Journey Information</h3>
+      <div>{result.split(",")[0]} minutes of travel time</div>
+      <div>{result.split(",")[1].replace(";", "")} miles of travel</div>
+    </>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Journey Entry</h1>
+      <input
+        type="text"
+        id="first-entry"
+        className="entry-input"
+        onChange={handleFirstEntryChange}
+      />
+      <input
+        type="text"
+        id="second-entry"
+        className="entry-input"
+        onChange={handleSecondEntryChange}
+      />
+      <button className="calculate-btn" onClick={handleJourneyCalculation}>
+        Calculate Journey
+      </button>
+
+      {result && displayResult()}
     </div>
   );
-}
+};
 
 export default App;
